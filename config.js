86478c13b839520b78bc3afac85268b2f52209b7
Eliott <eliottmieze@gmail.com>
2022-12-06 11:24:50 +0100

    token

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    token: "OTM4MTYxNzQ4ODMyMzA1MjI0.Go8JQv.NKnFXKVX_nAdoFD7gBnQDepiiix_2sF11uNsXg", // not bot
-	//token: "ODMxOTM4MTM5NTAwOTcwMDA3.GXCQvT.Bp4FQz2Dcb4edMQ4NlTYCOZxV-2-oIBpKZ9q7c", // nc bot
-	//token: "ODk4NDY5MTY0NjkwMzIxNDA4.GUPNN5.Asu_seOZbwkzcePE0zZ7rGNULiv_5rC4g3_zik", // bot lucas
-	clientId: "938161748832305224",
+    clientId: "938161748832305224",
 	guildId: "876217954688172062",
 }
